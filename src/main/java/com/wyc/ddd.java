c3c77b6aaabab8281c5ff2737d2fcc0bcb7be363
wyc <wyc@xujia.com>
2022-07-20 21:46:37 +0800

    second commit

--- src/main/java/com/wyc/ddd.java
+++ src/main/java/com/wyc/ddd.java
@@ -3,5 +3,6 @@ package com.wyc;
 public class ddd {
     public static void main(String[] args) {
         System.out.println("helllo first gittest");
+        System.out.println("hello second gittest");
     }
 }
