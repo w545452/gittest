26e4327f4b525237cc4a86d9e3b0855f0ba5d790
wyc <wyc@xujia.com>
2022-07-20 21:53:19 +0800

    hotfix first commit

--- src/main/java/com/wyc/ddd.java
+++ src/main/java/com/wyc/ddd.java
@@ -4,6 +4,6 @@ public class ddd {
     public static void main(String[] args) {
         System.out.println("helllo first gittest");
         System.out.println("hello second gittest");
-        System.out.println("hello third gittest");
+        System.out.println("hello third gittest4");
     }
 }
